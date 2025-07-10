c55b3c42cd9ce351d6c52523ab959f57db7b6101
thedrew94 <andrewlaurentiu94@gmail.com>
2025-07-10 12:19:39 +0200

    refactor search page components

--- frontend/src/components/SearchPage.jsx
+++ frontend/src/components/SearchPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { svgSelector } from '../utils/svgSelector';
 import { getFetch } from '../utils/getFetch';
 import { Link } from 'react-router-dom';
+import MainNavbar from './MainNavbar';
 
 const themes = {
   forHer: {
@@ -40,7 +41,7 @@ const themes = {
 
 export default function SearchPage() {
   const [theme, setTheme] = useState('forHer');
-  const [businessData, setBusinessData] = useState([]);
+  const [businessData, setBusinessData] = useState(null);
 
   function changeAppTheme({ theme = 'forHer' }) {
     setTheme(theme);
@@ -62,13 +63,14 @@ export default function SearchPage() {
 
   return (
     <div className="search_page">
+      <MainNavbar />
       <div className="bg_header">
         <h1>BarberApp</h1>
       </div>
       <div className="search_form">
         <div className="search_for">
           <button
-            className="stnd_btn"
+            className="stnd_btn stnd_btn_sltd"
             onClick={() => {
               changeAppTheme({ theme: 'forHer' });
             }}>
@@ -107,38 +109,194 @@ export default function SearchPage() {
         </form>
         {/* results */}
         <div>
-          <ul className="result_ul">
-            {businessData &&
-              businessData.map((bd, idx) => {
-                return (
-                  <li key={`shop_${idx}`} className="result_li">
-                    <div className="result_li_actions">
-                      <h2>Il nome del posto Cacca & Puppu 2</h2>
-                      <button>
-                        {svgSelector({ svgName: 'bookmark', svgWidth: '18px', svgHeight: '18px', svgFill: '#867c7c' })}
-                        <p>Bookmark this place</p>
-                      </button>
-                    </div>
-                    <div className="place_info">
-                      {/* img slider */}
-                      <div className="place_img">
-                        <img src="./src/assets/forher_bg.jpg" alt="" width="200px" height="200px" />
-                      </div>
-                      {/* info */}
-                      <div className="place_text">
-                        <h3 className="place_description">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, illum tempore quisquam repellendus, quidem nobis recusandae, tenetur dicta perferendis
-                          quaerat odit? Sed, doloremque provident! Aliquam magnam consequuntur harum maxime numquam.
-                        </h3>
-                        <Link to="/shop/123" className="place_open">
-                          Controlla disponibilita
-                        </Link>
-                      </div>
-                    </div>
-                  </li>
-                );
-              })}
-          </ul>
+          {businessData && (
+            <>
+              <div className="spotlight_title">
+                {svgSelector({ svgName: 'store', svgWidth: '28px', svgHeight: '28px', svgFill: '#481b1b' })}
+                <h6 className="h2">Risultati</h6>
+              </div>
+              <ul className="result_ul">
+                {businessData.length > 0 ? (
+                  businessData.map((bd, idx) => {
+                    return (
+                      // <li key={`shop_${idx}`} className="result_li">
+                      //   <div className="result_li_actions">
+                      //     <h2>Il nome del posto Cacca & Puppu 2</h2>
+                      //     <button>
+                      //       {svgSelector({ svgName: 'bookmark', svgWidth: '18px', svgHeight: '18px', svgFill: '#867c7c' })}
+                      //       <p>Bookmark this place</p>
+                      //     </button>
+                      //   </div>
+                      //   <div className="place_info">
+                      //     {/* img slider */}
+                      //     <div className="place_img">
+                      //       <img src="./src/assets/forher_bg.jpg" alt="" width="200px" height="200px" />
+                      //     </div>
+                      //     {/* info */}
+                      //     <div className="place_text">
+                      //       <h3 className="place_description">
+                      //         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, illum tempore quisquam repellendus, quidem nobis recusandae, tenetur dicta perferendis
+                      //         quaerat odit? Sed, doloremque provident! Aliquam magnam consequuntur harum maxime numquam.
+                      //       </h3>
+                      //       <Link to="/shop/123" className="place_open">
+                      //         Controlla disponibilita
+                      //       </Link>
+                      //     </div>
+                      //   </div>
+                      // </li>
+                      <li key={`shop_${idx}`}>
+                        <div className="spotlight_business">
+                          <img src="./src/assets/forher_bg.jpg" alt="" width="250px" height="250px" />
+                          <div className="spotlight_business_info_container">
+                            <div className="spotlight_business_info">
+                              {svgSelector({ svgName: 'store', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                              <h5 className="h4">Il barbiere Bastardo</h5>
+                            </div>
+                            <div className="spotlight_business_info">
+                              {svgSelector({ svgName: 'location', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                              <h5 className="h4">Roma via della marsala 9999</h5>
+                            </div>
+                            <div className="spotlight_business_info">
+                              {svgSelector({ svgName: 'feedback', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                              <h5 className="h4">4.4</h5>
+                            </div>
+                            <button className="spotlight_shops_btns">
+                              {svgSelector({ svgName: 'bookmark', svgWidth: '20px', svgHeight: '20px', svgFill: '#fff' })}
+                              <p className="h5">Aggiungi ai preferiti</p>
+                            </button>
+                            <button className="spotlight_shops_btns h5">
+                              {svgSelector({ svgName: 'booking', svgWidth: '20px', svgHeight: '20px', svgFill: '#fff' })}
+                              <p>Controlla disponibilita</p>
+                            </button>
+                          </div>
+                        </div>
+                      </li>
+                    );
+                  })
+                ) : (
+                  <h2 className="h4 nothing_found_text">Nessun risultato trovato...</h2>
+                )}
+              </ul>
+            </>
+          )}
+        </div>
+      </div>
+
+      {/* SPOTLIGHT SHOPS */}
+      <div className="spotlight_shops">
+        <div className="spotlight_title">
+          {svgSelector({ svgName: 'lamp', svgWidth: '28px', svgHeight: '28px', svgFill: '#481b1b' })}
+          <h6 className="h2">Sotto i riflettori</h6>
+        </div>
+        <ul className="spotlight_list">
+          <li>
+            <div className="spotlight_business">
+              <img src="./src/assets/forher_bg.jpg" alt="" width="250px" height="250px" />
+              <div className="spotlight_business_info_container">
+                <div className="spotlight_business_info">
+                  {svgSelector({ svgName: 'store', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                  <h5 className="h4">Il barbiere Bastardo</h5>
+                </div>
+                <div className="spotlight_business_info">
+                  {svgSelector({ svgName: 'location', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                  <h5 className="h4">Roma via della marsala 9999</h5>
+                </div>
+                <div className="spotlight_business_info">
+                  {svgSelector({ svgName: 'feedback', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                  <h5 className="h4">4.4</h5>
+                </div>
+                <button className="spotlight_shops_btns">
+                  {svgSelector({ svgName: 'bookmark', svgWidth: '20px', svgHeight: '20px', svgFill: '#fff' })}
+                  <p className="h5">Aggiungi ai preferiti</p>
+                </button>
+                <button className="spotlight_shops_btns h5">
+                  {svgSelector({ svgName: 'booking', svgWidth: '20px', svgHeight: '20px', svgFill: '#fff' })}
+                  <p>Controlla disponibilita</p>
+                </button>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="spotlight_business">
+              <img src="./src/assets/forher_bg.jpg" alt="" width="250px" height="250px" />
+              <div className="spotlight_business_info_container">
+                <div className="spotlight_business_info">
+                  {svgSelector({ svgName: 'store', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                  <h5 className="h4">Il barbiere Bastardo</h5>
+                </div>
+                <div className="spotlight_business_info">
+                  {svgSelector({ svgName: 'location', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                  <h5 className="h4">Roma via della marsala 9999</h5>
+                </div>
+                <div className="spotlight_business_info">
+                  {svgSelector({ svgName: 'feedback', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                  <h5 className="h4">4.4</h5>
+                </div>
+                <button className="spotlight_shops_btns">
+                  {svgSelector({ svgName: 'bookmark', svgWidth: '20px', svgHeight: '20px', svgFill: '#fff' })}
+                  <p className="h5">Aggiungi ai preferiti</p>
+                </button>
+                <button className="spotlight_shops_btns h5">
+                  {svgSelector({ svgName: 'booking', svgWidth: '20px', svgHeight: '20px', svgFill: '#fff' })}
+                  <p>Controlla disponibilita</p>
+                </button>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="spotlight_business">
+              <img src="./src/assets/forher_bg.jpg" alt="" width="250px" height="250px" />
+              <div className="spotlight_business_info_container">
+                <div className="spotlight_business_info">
+                  {svgSelector({ svgName: 'store', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                  <h5 className="h4">Il barbiere Bastardo</h5>
+                </div>
+                <div className="spotlight_business_info">
+                  {svgSelector({ svgName: 'location', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                  <h5 className="h4">Roma via della marsala 9999</h5>
+                </div>
+                <div className="spotlight_business_info">
+                  {svgSelector({ svgName: 'feedback', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+                  <h5 className="h4">4.4</h5>
+                </div>
+                <button className="spotlight_shops_btns">
+                  {svgSelector({ svgName: 'bookmark', svgWidth: '20px', svgHeight: '20px', svgFill: '#fff' })}
+                  <p className="h5">Aggiungi ai preferiti</p>
+                </button>
+                <button className="spotlight_shops_btns h5">
+                  {svgSelector({ svgName: 'booking', svgWidth: '20px', svgHeight: '20px', svgFill: '#fff' })}
+                  <p>Controlla disponibilita</p>
+                </button>
+              </div>
+            </div>
+          </li>
+        </ul>
+        {/* <div className="spotlight_business">
+          <img src="./src/assets/forher_bg.jpg" alt="" width="250px" height="250px" />
+          <div className="spotlight_business_info_container">
+            <div className="spotlight_business_info">
+              {svgSelector({ svgName: 'store', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+              <h5 className="h4">Il barbiere Bastardo</h5>
+            </div>
+            <div className="spotlight_business_info">
+              {svgSelector({ svgName: 'location', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+              <h5 className="h4">Roma via della marsala 9999</h5>
+            </div>
+            <div className="spotlight_business_info">
+              {svgSelector({ svgName: 'feedback', svgWidth: '24px', svgHeight: '24px', svgFill: '#481b1b' })}
+              <h5 className="h4">4.4</h5>
+            </div>
+            <h6 className="h6">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus inventore quis nulla cum quas corrupti quaerat voluptate magni! Quia quo impedit delectus
+              pariatur ut maiores sunt consequatur nobis, dicta consequuntur...
+            </h6>
+          </div>
+        </div> */}
+
+        <div className="spotlight_btns">
+          <button className="spotlight_act_btns">.</button>
+          <button className="spotlight_act_btns">.</button>
+          <button className="spotlight_act_btns">.</button>
         </div>
       </div>
     </div>
